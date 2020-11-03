273093551199671efbe33f5a62a26bd73e65b3d0
brendino500 <brenda.ty@live.com>
2020-11-03 19:15:08 +0000

    eslint and scss linked

--- src/Theme.js
+++ src/Theme.js
@@ -3,44 +3,45 @@ import { createMuiTheme } from '@material-ui/core/styles'
 export default createMuiTheme({
   palette: {
     primary: {
-      main: "#d3c9c0"
+      main: '#d3c9c0',
     },
     secondary: {
-      main: "#323e47"
+      main: '#323e47',
     },
     typography: {
-      fontFamily: "Libre Baskerville",
+      fontFamily: 'Libre Baskerville',
       h1: {
         fontSize: 42,
         letterSpacing: 10,
+        fontFamily: 'Libre Baskerville',
       },
       h2: {
-        fontFamily: "Libre Baskerville",
+        fontFamily: 'Libre Baskerville',
         fontSize: 30,
         letterSpacing: 5,
       },
       h3: {
-        fontFamily: "Libre Baskerville",
+        fontFamily: 'Libre Baskerville',
         fontSize: 25,
         letterSpacing: 2,
       },
       subtitle2: {
-        fontFamily: "Libre Baskerville",
+        fontFamily: 'Libre Baskerville',
         fontSize: 16,
         letterSpacing: 2,
       },
       p: {
-        fontFamily: "Work Sans",
+        fontFamily: 'Work Sans',
         fontSize: 18,
         letterSpacing: 1,
-      }
+      },
     },
     overrides: {
       MuiTypography: {
         text: {
-          fontFamily: "Libre Baskerville"
-        }
-      }
-    }
-  }
+          fontFamily: 'Libre Baskerville',
+        },
+      },
+    },
+  },
 })
