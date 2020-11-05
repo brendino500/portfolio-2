69c3c74c375960b470d8d6c5d8b0988219229b9f
brendino500 <brenda.ty@live.com>
2020-11-05 12:25:13 +0000

    Jungle københavn card final :heavy_check_mark:

--- src/components/projects/JungleKobenhavn.js
+++ src/components/projects/JungleKobenhavn.js
@@ -21,6 +21,7 @@ import {
   SiMaterialUi,
   SiTypescript,
   SiReact,
+  SiHeroku,
 } from 'react-icons/si'
 
 const useStyles = makeStyles((theme) => ({
@@ -78,8 +79,12 @@ const useStyles = makeStyles((theme) => ({
   techIcon: {
     color: '#d3c9c0',
     fontSize: 20,
-    margin: 3,
+    margin: 5,
     marginTop: '10%',
+  },
+  iconLayout: {
+    positon: 'absolute',
+    bottom: 0,
   },
 }))
 
@@ -110,52 +115,59 @@ export default function JungleKobenhavn() {
         </CardContent>
         <div className={classes.text}>
           <Typography className={classes.text}>
-            For this project, we really wanted to{' '}
-            <i>push ourselves with Material UI</i> (we definitely had to prove
-            our styling was better than in our Reactathon project... lol!) and{' '}
-            <i>learning Material UI was such a rewarding experience.</i> We also
-            implemented some <i>sexy parallax.</i>
+            After the 12 week bootcamp I wanted to{' '}
+            <i>challenge myself to make an ecommerce site.</i> This is the
+            closest thing I'll be to owning my own plant shop!
           </Typography>
           <Typography className={classes.text}>
-            We made our own Python Django API using REST Framework and Postgres
-            Database. Users can register, sign in, create a profile, follow
-            other users and upload their own photos.
+            This is also my first deployed project with TypeScript which was a
+            huge learning curve! I also worked with a designer at the beginning
+            as I enjoy being collaborative but mainly to have experience working
+            with a UX team member.
           </Typography>
         </div>
-        <Grid container justify="space-between">
-          <Grid>
-            <IconButton
-              href="https://github.com/brendino500/junglekobenhavn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GitHubIcon
-                className={('github-link', classes.profileLinks)}
-                fontSize="small"
-              />
-            </IconButton>
-            <IconButton
-              href="http://jungle-kobenhavn.herokuapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkIcon
-                className={('github-link', classes.profileLinks)}
-                fontSize="small"
-              />
-            </IconButton>
+        <div>
+          <Grid
+            container
+            justify="space-between"
+            className={classes.iconLayout}
+          >
+            <Grid>
+              <IconButton
+                href="https://github.com/brendino500/junglekobenhavn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon
+                  className={('github-link', classes.profileLinks)}
+                  fontSize="small"
+                />
+              </IconButton>
+              <IconButton
+                href="http://jungle-kobenhavn.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkIcon
+                  className={('github-link', classes.profileLinks)}
+                  fontSize="small"
+                />
+              </IconButton>
+            </Grid>
+            <Grid>
+              <SiJavascript className={classes.techIcon} />
+              <SiTypescript className={classes.techIcon} />
+              <SiMaterialUi className={classes.techIcon} />
+              <SiReact className={classes.techIcon} />
+              <SiHtml5 className={classes.techIcon} />
+              <SiCss3 className={classes.techIcon} />
+              <SiMongodb className={classes.techIcon} />
+              <SiNodeDotJs className={classes.techIcon} />
+              <i className="devicon-express-original skills-details-icon"></i>
+              <SiHeroku className={classes.techIcon} />
+            </Grid>
           </Grid>
-          <Grid>
-            <SiJavascript className={classes.techIcon} />
-            <SiTypescript className={classes.techIcon} />
-            <SiMaterialUi className={classes.techIcon} />
-            <SiReact className={classes.techIcon} />
-            <SiHtml5 className={classes.techIcon} />
-            <SiCss3 className={classes.techIcon} />
-            <SiMongodb className={classes.techIcon} />
-            <SiNodeDotJs className={classes.techIcon} />
-          </Grid>
-        </Grid>
+        </div>
       </div>
       <a
         href="http://jungle-kobenhavn.herokuapp.com/"
